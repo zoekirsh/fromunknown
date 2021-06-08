8c2722e91409c15784408412f7a877b9b37c0691
Zoe Kirsh <zoekirsh@gmail.com>
2021-06-07 17:54:07 -0700

    circle text successful

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import CircleType from 'circletype';
 
 const Landing = () => {
+  const circleText = useRef();
+
+  useEffect(() => {
+    new CircleType(circleText.current)
+  }, [])
+
+
   return (
-    <div className="center landing">
-      <div><Link to="/photo"><span>photo</span></Link></div>
-      <div><Link to="/ecomm"><span>e-commerce</span></Link></div>
-      <div><a target="_blank" rel="noreferrer" href="https://twitter.com/seeyounvr"><span>crypto</span></a></div>
-      <div><Link to="/art"><span>art</span></Link></div>
-      <div><Link to="/approach"><span>approach</span></Link></div>
+    <div ref={circleText} id="circletext" className="center landing">
+      <span><Link to="/photo">photo</Link></span>
+      <span className="black">_</span>
+      <span><Link to="/ecomm">e-commerce</Link></span>
+      <span className="black">_</span>
+      <span><a target="_blank" rel="noreferrer" href="https://twitter.com/seeyounvr">crypto</a></span>
+      <span className="black">_</span>
+      <span><Link to="/art">art</Link></span>
+      <span className="black">_</span>
+      <span><Link to="/approach">approach</Link></span>
+      <span className="black">_</span>
     </div>
   )
 }
